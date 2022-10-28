616c700496433b52de7acf2519ef0f43aec0bf7b
clarence <cgwhite76@gmail.com>
2022-10-28 09:17:08 -0500

    Final Classes Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+
+            myCar.Make = "Ford";
+            myCar.Model = "Mustang";
+            myCar.Year = 2018;
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model); 
+            Console.WriteLine(myCar.Year);  
+
+
         }
     }
 }
